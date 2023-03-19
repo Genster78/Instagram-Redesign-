4a6ae6f3776aea0e686c9b0a0f8fd90e21337c79
UdaraNilupul <19it489@itum.mrt.ac.lk>
2023-03-19 09:42:33 +0530

    profile details

--- lib/Pages/profile_page.dart
+++ lib/Pages/profile_page.dart
@@ -55,19 +55,182 @@ class _ProfilePageState extends State<ProfilePage> {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            // tab bar
-            TabBar(
-              tabs: tabs,
-            ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              // profiole details
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // following
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: const [
+                      Text(
+                        '365',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Following',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
 
-            //tab bar view
-            Expanded(
-              child: TabBarView(children: tabBarViews),
-            ),
-          ],
+                  // profile pic
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                  ),
+
+                  // followers
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        '365',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Following',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+              // name
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Udara Nilupul',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    '|',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    'UI Designer',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              // bio
+              Text(
+                'UI Designer ~ Flutter Developer ~ Graphic Designer',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 5),
+
+              // links
+              const Text(
+                'behance.net/PantherStudioDesign',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // buttons
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  children: [
+                    // edit profile
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Edit Profile',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(
+                      width: 10,
+                    ),
+
+                    // contact
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Contact',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+               const SizedBox(height: 20),
+              // tab bar
+              TabBar(
+                tabs: tabs,
+              ),
+
+              //tab bar view
+              Expanded(
+                child: TabBarView(children: tabBarViews),
+              ),
+            ],
+          ),
         ),
       ),
     );
