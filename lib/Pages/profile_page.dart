2c61901455b12fdcb50a0efed3ddc9f67065e5f5
UdaraNilupul <19it489@itum.mrt.ac.lk>
2023-03-19 08:35:05 +0530

    basic tabBar

--- lib/Pages/profile_page.dart
+++ lib/Pages/profile_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../tabs/feed_view.dart';
+import '../tabs/reels_view.dart';
+import '../tabs/tagged_view.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -8,8 +12,64 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  //tabs
+  final List<Widget> tabs = const [
+    // feed tab
+    Tab(
+      icon: Icon(
+        Icons.image,
+        color: Colors.grey,
+      ),
+    ),
+
+    // reels tab
+    Tab(
+      icon: Icon(
+        Icons.video_collection,
+        color: Colors.grey,
+      ),
+    ),
+
+    // tagged tab
+    Tab(
+      icon: Icon(
+        Icons.bookmark,
+        color: Colors.grey,
+      ),
+    ),
+  ];
+
+  final List<Widget> tabBarViews = [
+    // feed view
+    FeedView(),
+
+    // reels view
+    ReelsView(),
+
+    // tagged view
+    TaggedView(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            // tab bar
+            TabBar(
+              tabs: tabs,
+            ),
+
+            //tab bar view
+            Expanded(
+              child: TabBarView(children: tabBarViews),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
